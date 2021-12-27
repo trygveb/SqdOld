03534b45152ba0294ae614eef3271507dc8320e7
Trygve <trygve.botnen@gmail.com>
2021-12-27 14:52:09 +0000

    Working with Forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,39 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+               <div class="card-header">{{ __('Forgot Your Password?') }}</div>            
+               <div class="card-body">
+                
+            {{ __('No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<!--        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+         Validation Errors 
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />-->
+<br><br>
+               <form method="POST" action="{{ route('password.email') }}">
+                   @csrf
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                   <!-- Email Address -->
+                  <div>
+                       <label for="email" >{{__('E-Mail Address')}}</label>
+                       <input id="email" class="form-control block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                  </div>
+                   <br>
+<!--                  <div class="form-group row mb-0">
+                     <div class="col-md-8 offset-md-4">-->
+                        <x-submit-button submitText="{{ __('Send Password Reset Link')}}" cancelText="{{ __('Cancel') }}"/>
+<!--                     </div>                   
+                  </div>-->
+               </form>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+         </div>
+      </div>
+   </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,70 +5,94 @@ use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 Route::group(
-   ['prefix' => LaravelLocalization::setLocale(),
-   'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function()
-{
+        ['prefix' => LaravelLocalization::setLocale(),
+            'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
 //   Route::get('/', function () {
 //       return view('welcome');
 //   });
-
 //   Route::get('/dashboard', function () {
 //       return view('dashboard');
 //   })->middleware(['auth'])->name('dashboard');
+           //require __DIR__.'/auth.php';
 
-   //require __DIR__.'/auth.php';
+           Route::get('login/{app}', [CustomAuthController::class, 'index'])->name('login');
+           Route::get('registration/{app}', [CustomAuthController::class, 'registration'])->name('register-user');
+           Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+           Route::get('welcome', [HomeController::class, 'welcome'])->name('welcome')->middleware('auth'); // For sqd.se, logged in, application not selected
+           Route::get('/email/showVerifyEmail/{app}', [CustomAuthController::class, 'showVerifyEmail'])->name('verification.notice');
 
-   Route::get('login/{app}', [CustomAuthController::class, 'index'])->name('login');
-   Route::get('registration/{app}', [CustomAuthController::class, 'registration'])->name('register-user');
-   Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
-   Route::get('welcome', [HomeController::class, 'welcome'])->name('welcome')->middleware('auth'); // For sqd.se, logged in, application not selected
-   Route::get('/email/showVerifyEmail/{app}', [CustomAuthController::class, 'showVerifyEmail'])->name('verification.notice');
+           Route::get('/', [HomeController::class, 'home'])->name('home'); // For sqd.se, NOT logged in, application not selected
+           Route::get('calls', [HomeController::class, 'callsGuest'])->name('calls.guest');
+           Route::get('schema', [HomeController::class, 'schemaGuest'])->name('schema.guest');
+           Route::get('/calls/home', [HomeController::class, 'callsHome'])->name('calls.home')->middleware('auth');
+           Route::get('/schema/home', [HomeController::class, 'schemaHome'])->name('schema.home')->middleware('auth');
+           Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
 
-   Route::get('/', [HomeController::class, 'home'])->name('home'); // For sqd.se, NOT logged in, application not selected
-   Route::get('calls', [HomeController::class, 'callsGuest'])->name('calls.guest');
-   Route::get('schema', [HomeController::class, 'schemaGuest'])->name('schema.guest');
-   Route::get('/calls/home', [HomeController::class, 'callsHome'])->name('calls.home')->middleware('auth');
-   Route::get('/schema/home', [HomeController::class, 'schemaHome'])->name('schema.home')->middleware('auth');
-   Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+           Route::get('/forgot-password', function () {
+              return view('auth.forgot-password');
+           })->middleware('guest')->name('password.request');
 
-   Route::get('/forgot-password', function () {
-    return view('auth.forgot-password');
-   })->middleware('guest')->name('password.request');   
-Route::post('/forgot-password', function (Request $request) {
-    $request->validate(['email' => 'required|email']);
+           Route::post('/forgot-password', function (Request $request) {
+              $request->validate(['email' => 'required|email']);
+              $status = Password::sendResetLink(
+                              $request->only('email')
+              );
+              return $status === Password::RESET_LINK_SENT ? back()->with(['status' => __($status)]) : back()->withErrors(['email' => __($status)]);
+           })->middleware('guest')->name('password.email');
 
-    $status = Password::sendResetLink(
-        $request->only('email')
-    );
+           Route::get('/reset-password/{token}', function ($token) {
+              return view('auth.reset-password', ['token' => $token]);
+           })->middleware('guest')->name('password.reset');
 
-    return $status === Password::RESET_LINK_SENT
-                ? back()->with(['status' => __($status)])
-                : back()->withErrors(['email' => __($status)]);
-   })->middleware('guest')->name('password.email');   
+           Route::post('/reset-password', function (Request $request) {
+              $request->validate([
+                  'token' => 'required',
+                  'email' => 'required|email',
+                  'password' => 'required|min:8|confirmed',
+              ]);
+
+              $status = Password::reset(
+                              $request->only('email', 'password', 'password_confirmation', 'token'),
+                              function ($user, $password) {
+                                 $user->forceFill([
+                                     'password' => Hash::make($password)
+                                 ])->setRememberToken(Str::random(60));
+
+                                 $user->save();
+
+                                 event(new PasswordReset($user));
+                              }
+              );
+
+              return $status === Password::PASSWORD_RESET ? redirect()->route('login')->with('status', __($status)) : back()->withErrors(['email' => [__($status)]]);
+           })->middleware('guest')->name('password.update');
 //   Route::post('/email/verification-notification', [CustomAuthController::class, 'sendEmailVerificationNotification'])
 //   ->middleware(['auth', 'throttle:6,1'])
 //   ->name('verification.send');
-}
+        }
 );
 
 Route::post('/email/verification-notification', [CustomAuthController::class, 'sendEmailVerificationNotification'])
-   ->middleware(['auth', 'throttle:6,1'])
-   ->name('verification.send');
+        ->middleware(['auth', 'throttle:6,1'])
+        ->name('verification.send');
 
-
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
 Route::get('/switchLocale', [HomeController::class, 'switchLocale'])->name('switchLocale');
 
 //  Route::get('/email/verify', function () { return view('auth.verify-email');
@@ -76,10 +100,9 @@ Route::get('/switchLocale', [HomeController::class, 'switchLocale'])->name('swit
 
 
 Route::get('/email/verify/{id}/{hash}/{application}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect(route($request->application.'.home'));
-})
+           $request->fulfill();
+           return redirect(route($request->application . '.home'));
+        })
 //   ->middleware(['auth', 'signed'])
-   ->name('verification.verify');
-
+        ->name('verification.verify');
 
