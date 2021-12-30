90197842c9c2f1d3da7b02e546bcbbe905ca172c
Trygve <trygve.botnen@gmail.com>
2021-12-30 20:13:05 +0000

    Working with forgotten password

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -34,21 +34,22 @@ class AuthServiceProvider extends ServiceProvider {
       $this->registerPolicies();
       VerifyEmail::toMailUsing(function ($notifiable, $url) {
          return (new MailMessage)
-            ->subject(Lang::get('Verify email address on') . ' ' . config('app.name'))
-            ->line(Lang::get('Please click the button below to verify your email address.'))
-            ->action(Lang::get('Verify Email Address'), $url)
-            ->salutation(new HtmlString(Lang::get('Regards') . '<br>Trygve Botnen, administratör på ' . config('app.name')))
-            ->line(Lang::get('If you did not create an account, no further action is required.'));
+                 ->subject(Lang::get('Verify email address on') . ' ' . config('app.name'))
+                 ->line(Lang::get('Please click the button below to verify your email address.'))
+                 ->action(Lang::get('Verify Email Address'), $url)
+                 ->salutation(new HtmlString(Lang::get('Regards') . '<br>Trygve Botnen, administratör på ' . config('app.name')))
+                 ->line(Lang::get('If you did not create an account, no further action is required.'));
       });
       ResetPassword::toMailUsing(function ($notifiable, $url) {
+         $url= config('app.frontend_url').'/reset-password/'.$url;
 //         dd($url);
          return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification for'). ' ' . config('app.name'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->salutation(new HtmlString(Lang::get('Regards') . '<br>Trygve Botnen, '.Lang::get('administrator at').' ' . config('app.name')))
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+                 ->subject(Lang::get('Reset Password Notification for') . ' ' . config('app.name'))
+                 ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+                 ->action(Lang::get('Reset Password'), $url)
+                 ->salutation(new HtmlString(Lang::get('Regards') . '<br>Trygve Botnen, ' . Lang::get('administrator at') . ' ' . config('app.name')))
+                 ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
+                 ->line(Lang::get('If you did not request a password reset, no further action is required.'));
       });
 
 //      // Adds current application (schema or call) to the url used in the verify email email,
@@ -69,26 +70,6 @@ class AuthServiceProvider extends ServiceProvider {
 //                 ]
 //         );
 //      });
-//      // Adds current application (schema or call) to the url used in the reset password email,
-//      //  in order to return to the current application
-//      ResetPassword::createUrlUsing(function ($user) {
-////         dd('ResetPassword::createUrlUsing user=' . $user);
-////         if (is_null($user->application)) {
-////            $user->application = 'schema';
-////         }
-//
-//         //$user->application='xxx';
-//         return URL::temporarySignedRoute(
-//                 'verification.verify',
-//                 Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
-//                 [
-//                     'id' => $user->getKey(),
-//                     'hash' => sha1($user->getEmailForVerification()),
-////                     'application' => $user->application,
-//                     'application' => 'schema'
-//                 ]
-//         );
-//      });
-   }
 
+   }
 }
