9a3cce1d3de24a830b5e7bc843ab8f2b6412dd19
Trygve <trygve.botnen@gmail.com>
2022-03-04 15:09:36 +0000

    Fixed flags for other browsers than Firefox

--- resources/views/components/flags-div.blade.php
+++ resources/views/components/flags-div.blade.php
@@ -1,4 +1,5 @@
 <div class="flags" style="font-weight: bold;">
-   <a href="{{ LaravelLocalization::getLocalizedURL('sv', null, [], true) }}"> {{country_flag('SE');}}</a>
-   <a href="{{ LaravelLocalization::getLocalizedURL('en', null, [], true) }}"> {{country_flag('GB');}}</a>
+   <a href="{{ LaravelLocalization::getLocalizedURL('sv', null, [], true) }}"> <img src="{{asset('images/swedish_flag.png')}}" alt="SE"></a>
+   <a href="{{ LaravelLocalization::getLocalizedURL('en', null, [], true) }}"> <img src="{{asset('images/british_flag.png')}}" alt="GB"></a>
+
 </div>
