b9aac102ad310d9e3fd772b27087b963781f636a
Trygve <trygve.botnen@gmail.com>
2022-03-05 07:21:53 +0000

    Added error page 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Underåll pågår</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #fff; background-color:#222; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Schemat kommer snart upp igen!</h1>
+    <div>
+        <p>Ursäkta avbrottet, men uppdatering av SdSchema pågår för närvarande. Försök igen om en stund!
+        <p>&mdash; Trygve</p>
+    </div>
+</article>
