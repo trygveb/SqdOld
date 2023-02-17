d55b04d74b017ca4c9cd8be457e52213e3f4efb1
vagrant <vagrant@homestead>
2023-02-17 13:55:56 +0000

    Fixed bug in right side  navbar

--- resources/views/components/right-side-of-navbar.blade.php
+++ resources/views/components/right-side-of-navbar.blade.php
@@ -16,7 +16,7 @@
    @auth
       <li class="nav-item dropdown">
          <a id="navbarDropdown" class="nav-link dropdown-toggle text-dark" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }}
+            {{ Auth::user()->complete_name }}
          </a>
          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="{{route('schedule.showMySchemas')}}" >{{ __('Schedules') }}
