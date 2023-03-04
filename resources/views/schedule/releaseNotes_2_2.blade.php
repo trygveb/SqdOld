7ea3feedb5e0b932ebaf6bc10c883b2c858adcb8
vagrant <vagrant@homestead>
2023-03-04 14:32:17 +0000

    Working with release notes

--- resources/views/schedule/releaseNotes_2_2.blade.php
+++ resources/views/schedule/releaseNotes_2_2.blade.php
@@ -4,9 +4,18 @@
 
 
 <div class="container">
-   <div class="col-md-12 text-center">
+    <h1 class="text-center"> {{ __('Release notes')}} for {{$names['application']}}  Version 2.2 </h1>
+     <div style="margin-left:auto;margin-right:auto;  width: 60%;  border: 3px solid #73AD21;  padding: 10px;">
 
-      <h1 class="text-center"> {{ __('Release notes')}} for {{$names['application']}}  Version 2.2 </h1>
+      <h2>Administration</h2>
+      <ul>
+          <li>Sidan för att skapa nytt schema uppdaterad</li>
+         <ul>
+             <li>Fält för beskrivning</li>
+            <li>Fält för "Namn i schema" för den som skapar schemat</li>
+         </ul>
+      </ul>
+    
    </div>    
 </div>
 @endsection
