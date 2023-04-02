766e5d9532d9dfae104079dfae1ca5520c70c964
vagrant <vagrant@homestead>
2023-04-02 19:42:38 +0000

    Test version of schedule_admin_menu.blade

--- resources/views/components/schedule-admin-menu.blade.php
+++ resources/views/components/schedule-admin-menu.blade.php
@@ -3,5 +3,7 @@
    </a>
    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
    <a class="dropdown-item" href="{{route('schedule.showComments',['scheduleId' => $scheduleId])}}">{{__('Manage comments')}}</a>
+   <a class="dropdown-item" href="{{route('schedule.showAddRemoveDates',['scheduleId' => $scheduleId])}}">{{__('Manage dates')}}</a>
+
    <a class="dropdown-item" href="{{route('schedule.showMembers',['scheduleId' => $scheduleId])}}">{{__('Manage members')}}</a>
    </div>
